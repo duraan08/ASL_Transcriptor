879c4e3d070c36686c2a94a0bc05d34d33860fe1
unai.duran <62382060+duraan08@users.noreply.github.com>
2023-12-19 14:02:08 +0100

    Update dataLoader.py

--- scripts/dataLoader.py
+++ scripts/dataLoader.py
@@ -2,70 +2,60 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
+from torch.nn.utils.rnn import pad_sequence
 import h5py
+import json
 
 # Dataset personalizado para cargar los datos
 class CustomDataset(Dataset):
-    def __init__(self, inputs, targets):
-        self.inputs = inputs
-        self.targets = targets
+    def __init__(self, num_classes, file_path):
+        self.inputs, self.targets, self.mask = load_hdf5_data(file_path)
+        self.num_classes = num_classes
 
     def __len__(self):
         return len(self.inputs)
 
     def __getitem__(self, idx):
-        return self.inputs[idx], self.targets[idx]
+        return self.inputs[idx], F.one_hot(self.targets[idx], num_classes=self.num_classes)
 
 # Función para cargar datos desde un archivo HDF5
 def load_hdf5_data(file_path):
     hdf5_file = h5py.File(file_path, 'r')
+    train_data = []
+    train_targets = []
+    train_mask = []
+    fichero = open('C:/Universidad/TFG/Desarrollo/index/Mapeo.json') ## Mapeo clases
+    fichero_json = json.load(fichero)
+    ## Ficheros .json (Mapeo de cada gloss --> book = 0, ... || Acceso rapido --> 00000.mp4 = 0 [book])
+    ## Aqui solo se necesita el segundo archivo
     for video in hdf5_file:
         print(f'Video encontrado --> {video}')
-        train_data = torch.tensor(hdf5_file[video][:])
+        train_data.append(torch.tensor(hdf5_file[video][:], dtype=torch.float))
+        train_mask.append(torch.zeros(train_data[-1].size(0)))
+        train_targets.append(fichero_json[video]) ##La clase que sea [0, 2, 5] 0: book, 2: car, ...
 
-    return train_data
-
-# # Ruta al archivo HDF5
-# file_path = 'C:/Universidad/TFG/Desarrollo/data_vector/landmarks_dataset.hdf5'
-
-# # Cargar datos desde el archivo HDF5
-# train_data = load_hdf5_data(file_path)
+    hdf5_file.close()
+    return train_data, train_targets, train_mask
 
 # Funcion para crear los dataLoaders que se usaran en el transformer
-def createDataLoaders(train_data):
-    # Determinar el porcentaje para el conjunto de validación
-    val_percentage = 0.2
-    total_samples = train_data.size(0)
-    print(f"Longitud datos de entrenamiento antes de separar --> {total_samples}")
-    num_val_samples = int(total_samples * val_percentage)
-
-    # Separar los datos en conjuntos de entrenamiento y validación
-    val_data = train_data[:num_val_samples]
-    train_data = train_data[num_val_samples:]
-
-    # Dividir los datos en inputs y targets (asumiendo que están concatenados)
-    train_inputs, train_targets = train_data[:, :-1], train_data[:, -1]
-    val_inputs, val_targets = val_data[:, :-1], val_data[:, -1]
-
+def createDataLoaders(num_classes, file_path):
     # Crear datasets y dataloaders
-    train_dataset = CustomDataset(train_inputs, train_targets)
-    val_dataset = CustomDataset(val_inputs, val_targets)
+    dataset = CustomDataset(num_classes, file_path)
 
     batch_size = 32
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size)
 
-    # # Verificar la creación exitosa de los DataLoaders
-    # for inputs, targets in train_loader:
-    #     print(f"Batch de entrenamiento - Inputs: {inputs.shape}, Targets: {targets.shape}")
-    #     print(f"\nPrimer lote de entrenamiento - Inputs: {inputs}")
-    #     print(f"\nPrimer lote de entrenamiento - Targets: {targets}")
-    #     break
+    def collate_fn_padd(batch):
+        data = [x[0] for x in batch]
+        data = pad_sequence(data, batch_first=False, padding_value=-2.0)
 
-    # for inputs, targets in val_loader:
-    #     print(f"Batch de validacion - Inputs: {inputs.shape}, Targets: {targets.shape}")
-    #     print(f"\nPrimer lote de validacion - Inputs: {inputs}")
-    #     print(f"\nPrimer lote de validacion - Targets: {targets}")
-    #     break
+        targets = [x[1] for x in batch]
+        targets = torch.tensor(targets)
 
-    return train_dataset, train_loader, val_dataset, val_loader
+        mask = [x[2] for x in batch]
+        mask = pad_sequence(mask, batch_first=False, padding_value=1)
+
+        return data, targets, mask
+
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn = collate_fn_padd)
+
+    return loader
