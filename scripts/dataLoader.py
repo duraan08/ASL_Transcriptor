9ab6b2bdf94e934a9a99bb13b6c7c8437bec1041
unai.duran <62382060+duraan08@users.noreply.github.com>
2023-12-29 15:33:15 +0100

    Update dataLoader.py

--- scripts/dataLoader.py
+++ scripts/dataLoader.py
@@ -5,6 +5,8 @@ from torch.utils.data import DataLoader, Dataset
 from torch.nn.utils.rnn import pad_sequence
 import h5py
 import json
+import os
+from json_creator import createMapeo
 
 # Dataset personalizado para cargar los datos
 class CustomDataset(Dataset):
@@ -16,24 +18,36 @@ class CustomDataset(Dataset):
         return len(self.inputs)
 
     def __getitem__(self, idx):
-        return self.inputs[idx], F.one_hot(self.targets[idx], num_classes=self.num_classes)
+        target = torch.tensor(self.targets[idx])
+        one_hot_target = F.one_hot(target, num_classes=self.num_classes)
+
+        return self.inputs[idx], one_hot_target, self.mask[idx]
 
 # Función para cargar datos desde un archivo HDF5
 def load_hdf5_data(file_path):
     hdf5_file = h5py.File(file_path, 'r')
+    
     train_data = []
     train_targets = []
     train_mask = []
-    fichero = open('C:/Universidad/TFG/Desarrollo/index/Mapeo.json') ## Mapeo clases
+    
+    path_fichero = 'C:/Universidad/TFG/Desarrollo/index/Mapeo.json'
+    if (not os.path.exists(path_fichero)):
+        createMapeo()
+    
+    fichero = open(path_fichero) ## Mapeo clases
     fichero_json = json.load(fichero)
+
     ## Ficheros .json (Mapeo de cada gloss --> book = 0, ... || Acceso rapido --> 00000.mp4 = 0 [book])
     ## Aqui solo se necesita el segundo archivo
+
     for video in hdf5_file:
         print(f'Video encontrado --> {video}')
         train_data.append(torch.tensor(hdf5_file[video][:], dtype=torch.float))
-        train_mask.append(torch.zeros(train_data[-1].size(0)))
-        train_targets.append(fichero_json[video]) ##La clase que sea [0, 2, 5] 0: book, 2: car, ...
+        train_mask.append(torch.zeros(train_data[-1].size(0) + 1))
+        train_targets.append(int(fichero_json[video])) ##La clase que sea [0, 2, 5] 0: book, 2: car, ...
 
+    #print(f"Prueba dataLoader --> {train_targets}")
     hdf5_file.close()
     return train_data, train_targets, train_mask
 
@@ -47,12 +61,15 @@ def createDataLoaders(num_classes, file_path):
     def collate_fn_padd(batch):
         data = [x[0] for x in batch]
         data = pad_sequence(data, batch_first=False, padding_value=-2.0)
+        #print(f"Datos --> {data.size()}")
 
         targets = [x[1] for x in batch]
-        targets = torch.tensor(targets)
+        targets = torch.stack(targets).float()  # Convertir la lista de tensores en un solo tensor
+        #print(f"Targets --> {targets.size()}")
 
         mask = [x[2] for x in batch]
         mask = pad_sequence(mask, batch_first=False, padding_value=1)
+        #print(f"Mascara --> {mask.size()}")
 
         return data, targets, mask
 
