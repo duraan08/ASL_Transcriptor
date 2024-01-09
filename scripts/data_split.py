5743b58628984dde7ed0755fc97241d1e0ce0051
unai.duran <62382060+duraan08@users.noreply.github.com>
2024-01-09 16:22:13 +0100

    Update data_split.py

--- scripts/data_split.py
+++ scripts/data_split.py
@@ -3,9 +3,9 @@ import os
 import shutil
 
 ## Se obtiene el fichero que contiene los metadatos de los videos como el id, url ...
-fichero = open('C:/Universidad/TFG/Desarrollo/index/WLASL.json')
+fichero = open('/scratch/uduran005/tfg-workspace/index/WLASL.json')
 ## Se obtiene el directorio que almacena todos los videos 
-lista_videos = os.listdir('C:/Universidad/TFG/Desarrollo/ASL_videos/all_videos')
+lista_videos = os.listdir('/scratch/uduran005/tfg-workspace/ASL_videos/all_videos')
 
 ## Se carga en su formato JSON
 fichero_json = json.load(fichero)
@@ -28,13 +28,24 @@ for v in index_video:
     for video in lista_videos:
         if (v[0] == video):
             if (v[1] == "train"):
-                shutil.copy('C:/Universidad/TFG/Desarrollo/ASL_videos/' + video, 
-                            'C:/Universidad/TFG/Desarrollo/ASL_videos/train/' + video)
+                shutil.copy('/scratch/uduran005/tfg-workspace/ASL_videos/' + video, 
+                            '/scratch/uduran005/tfg-workspace/ASL_videos/train/' + video)
             elif (v[1] == "test"):
-                shutil.copy('C:/Universidad/TFG/Desarrollo/ASL_videos/' + video, 
-                            'C:/Universidad/TFG/Desarrollo/ASL_videos/test/' + video)
+                shutil.copy('/scratch/uduran005/tfg-workspace/ASL_videos/' + video, 
+                            '/scratch/uduran005/tfg-workspace/ASL_videos/test/' + video)
             elif (v[1] == "val"):
-                shutil.copy('C:/Universidad/TFG/Desarrollo/ASL_videos/' + video, 
-                            'C:/Universidad/TFG/Desarrollo/ASL_videos/val/' + video)
+                shutil.copy('/scratch/uduran005/tfg-workspace/ASL_videos/' + video, 
+                            '/scratch/uduran005/tfg-workspace/ASL_videos/val/' + video)
             else:
                 print(f"Existe otro tipo --> {v[1]}")
+
+## Se crea una primera muestra de 1000 datos de la carpeta train
+#lista_videos_train = os.listdir('/scratch/uduran005/tfg-workspace/ASL_videos/train')
+#count = 0
+#for video in lista_videos_train:
+#    if (count == 1000):
+#        print(f"Carga completada")
+#        break
+#    else:
+#        shutil.copy('/scratch/uduran005/tfg-workspace/ASL_videos/train/' + video, '/scratch/uduran005/tfg-workspace/ASL_videos/lil_test/' + video)
+#        count += 1
